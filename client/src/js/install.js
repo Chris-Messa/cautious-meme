86bcc41ca9738185e22e3342efcd5d89a92966fc
Chris Messa <blindsun95@gmail.com>
2023-08-05 20:45:13 -0500

    event handler for before install prompt event

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,8 +1,15 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let defPrompt
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault()
+
+    defPrompt = event
+
+    butInstall.style.display = 'block'
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});
